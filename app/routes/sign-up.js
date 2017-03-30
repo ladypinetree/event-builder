a58d28c8b8a7d2b4cff29b8c4bc5e10cd0f195f8
ladypinetree <maureenpine@gmail.com>
2017-03-30 15:17:38 -0400

    Styles event box   -gradient and shadow Fixes sign up form bug   -clears form on sign in
    Next Steps:
      - Remove transition to 'application' from cancel functions

--- app/routes/sign-up.js
+++ app/routes/sign-up.js
@@ -13,6 +13,9 @@ export default Ember.Route.extend({
       .then(() => this.get('auth').signIn(credentials))
       .then(() => this.transitionTo('application'))
       .then(() => {
+        credentials.password = null;
+        credentials.email = null;
+        credentials.passwordConfirmation = null;
         this.get('flashMessages')
         .success('Successfully signed-up! You have also been signed-in.');
       })
