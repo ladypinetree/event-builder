4f414eb56474310d9780344649c31e9634b82ede
ladypinetree <maureenpine@gmail.com>
2017-04-26 11:25:42 -0400

    Add in items to single event listing   - Change gradient to grey   - Add in shadow on footer.
    Next Steps:
      - Modify create and update scripts
      - Try to toggle done like listr-app
      - Event date display
      - About me page
      - Instructions on home screen

--- app/models/event.js
+++ app/models/event.js
@@ -4,6 +4,8 @@ export default DS.Model.extend({
   eventTitle: DS.attr('string'),
   eventType: DS.attr('string'),
   eventDate: DS.attr('date'),
+  todo1: DS.attr('string'),
+  todo2: DS.attr('string'),
   // user: DS.belongsTo('user'),
   // editable: DS.attr('boolean')
 });
